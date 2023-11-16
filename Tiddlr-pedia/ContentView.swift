f9c854b431b0703af99883b5c70f91e5f4b2f9b6
Renzo Sy <renzogabrielsy@gmail.com>
2023-11-16 10:19:30 +0800

    Initial Commit

---
+++ Tiddlr-pedia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tiddlr-pedia
+//
+//  Created by Renzo Sy on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tiddlr-pedia/Tiddlr_pediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiddlr_pediaApp.swift
+//  Tiddlr-pedia
+//
+//  Created by Renzo Sy on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Tiddlr_pediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
